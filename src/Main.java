9df90875243ec57a9ae35bb6716ff989fe5911b4
Sandeep Yerra <sandeep.yvs@gmail.com>
2023-02-26 22:00:50 +0530

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+
+            URL url = new URL("http://localhost:8080/movies");
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            //Getting the response code
+            int responsecode = conn.getResponseCode();
+
+            if (responsecode != 200) {
+                throw new RuntimeException("HttpResponseCode: " + responsecode);
+            } else {
+                System.out.println("Connection sucessfull");
+                String inline = "";
+                Scanner scanner = new Scanner(url.openStream());
+
+                //Write all the JSON data into a string using a scanner
+                while (scanner.hasNext()) {
+                    inline += scanner.nextLine();
+                }
+
+                //Close the scanner
+                scanner.close();
+
+                //Using the JSON simple library parse the string into a json object
+                JSONParser parse = new JSONParser();
+                Object data_obj = parse.parse(inline);
+
+                System.out.print(data_obj);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
